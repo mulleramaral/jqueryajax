cb2f754203d734209782bc9a3f527c35e81811e6
miiller caetano do amaral <miilleramaral@gmail.com>
2016-12-17 14:03:20 -0200

    Curso Concluido

--- js/main.js
+++ js/main.js
@@ -1,25 +1,59 @@
 $(function () {
+
+    var requestList = $.ajax({
+        method:"GET",
+        url:"post.php",
+        data:{listAll:"list"},
+        dataType:"json"
+    });
+
+    requestList.done(function(lista) {
+        console.log(lista);
+        var table= '<thead><tr><th>#</th><th>Nome</th><th>Email</th><th>Telefone</th></tr></thead><tbody>';
+        for(var i in lista){
+            table += '<tr><th scope="row">'+lista[i].id+'</th>';
+            table += '<td>'+lista[i].nome+'</td>';
+            table += '<td>'+lista[i].email+'</td>';
+            table += '<td>'+lista[i].telefone+'</td></tr>';
+        }
+        table += '</tbody>';
+        $('#contatos').html(table);
+    });
+
     $('#AjaxRequest').submit(function(){
 
-        var formArray = $(this).serializeArray();
+        var form = $(this).serialize();
 
         var request;
         request = $.ajax({
             method: "POST",
             url: "post.php",
-            data:formArray
+            data:form,
+            dataType:"json"
         });
 
         request.done(function(e){
-            console.log(e);
+            $("#msg").html(e.msg);
+
+            if(e.status){
+                $('#AjaxRequest').each(function () {
+                    this.reset();
+                })
+
+                var table = '<tr><th scope="row">'+ e.contato.id+'</th>';
+                table += '<td>'+e.contato.nome+'</td>';
+                table += '<td>'+e.contato.email+'</td>';
+                table += '<td>'+e.contato.telefone+'</td></tr>';
+                $('#contatos tbody').prepend(table);
+            }
         });
 
         request.fail(function(e){
-           console.log(e);
+            console.log("fail" + e);
         });
 
-        request.always(function(e){
-            console.log(e);
+        request.always(function () {
+            console.log("always");
         });
 
         return false;
--- post.php
+++ post.php
@@ -1,10 +1,65 @@
 <?php
 
 if($_GET){
-    var_dump($_GET);exit;
+    $data = listAll();
+    echo json_encode($data);exit;
 }
 
 if($_POST){
-    var_dump($_POST);exit;
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    if($nome == ""){
+        echo json_encode(['status'=>false,'msg'=>'Preencha o campo nome']);exit;
+    }
+
+    if($email == ""){
+        echo json_encode(['status'=>false,'msg'=>'Preencha o campo email']);exit;
+    }
+
+    if($telefone == ""){
+        echo json_encode(['status' =>false,'msg'=>'Preencha o campo telefone']);exit;
+    }
+
+    $id = save($_POST);
+    if($id){
+        $data = find($id);
+        echo json_encode(['status'=>true,'msg'=>"Sucesso! id: {$id}","contato"=>$data]);exit;
+    }else{
+        echo json_encode(['status'=>false,'msg'=>'Erro!']);exit;
+    }
 }
 
+function conn(){
+    $conn = new \PDO("mysql:host=localhost;dbname=ajax_jquery","root","");
+    return $conn;
+}
+
+function save($data){
+    $db = conn();
+    $query = "INSERT INTO contatos(nome,email,telefone) values (:nome,:email,:telefone)";
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(':nome',$data['nome']);
+    $stmt->bindValue(':email',$data['email']);
+    $stmt->bindValue(':telefone',$data['telefone']);
+    $stmt->execute();
+    return $db->lastInsertId();
+}
+
+function listAll(){
+    $db = conn();
+    $query = "SELECT * FROM contatos ORDER BY id DESC";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function find($id){
+    $db = conn();
+    $query = "SELECT * FROM contatos WHERE id=:id";
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(':id',$id);
+    $stmt->execute();
+    return $stmt->fetch(\PDO::FETCH_ASSOC);
+}
